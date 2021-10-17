8ad87f1e54220a8dd367f50b53f5c798d5436418
JUNGHOJONG <hojong1351@naver.com>
2021-10-17 19:06:03 +0900

    스프링 타입 컨버터 소개

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,47 @@
+package hello.typeconverter.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        log.info("V1 - intValue = {}", intValue);
+
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("V2 - intValue = {}", data);
+
+        return "ok";
+    }
+
+    @GetMapping("hello-v3")
+    public String helloV3(@ModelAttribute UserData data) {
+        log.info("V3 - data = {}", data.getData());
+
+        return "ok";
+    }
+
+    @Data
+    public static class UserData {
+        Integer data;
+    }
+
+    @GetMapping("hello-v4/users/{userId}")
+    public String helloV4(@PathVariable Integer userId) {
+        log.info("V4 - data = {}", userId);
+
+        return "ok";
+    }
+}
